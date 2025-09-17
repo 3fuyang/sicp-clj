36f3c64c68e681f5f8e3110b69be45608a7a5c84
fuyang <fuyang@shengwang.cn>
2025-09-17 21:55:20 +0800

    feat: update

--- src/sicp_clj/ch_1/gcd.clj
+++ src/sicp_clj/ch_1/gcd.clj
@@ -5,3 +5,18 @@
   (if (= b 0)
    a
    (gcd b (mod a b))))
+
+;; applicative-order eval
+;; (gcd 206 40)
+;; (gcd 40 (mod 206 40))
+;; (gcd 40 6)
+;; (gcd 6 (mod 40 6))
+;; (gcd 6 4)
+;; (gcd 4 (mod 6 4))
+;; (gcd 4 2)
+;; (gcd 2 (mod 4 2))
+;; (gcd 2 0)
+;; 2
+
+;; normal-order eval
+;; TODO
