6440ec247b9cfd84379e6dbba4bfbf3ddbeea0ed
fuyang <fuyang@agora.io>
2025-10-09 23:26:14 +0800

    feat: update

--- src/sicp_clj/ch_2/2_20.clj
+++ src/sicp_clj/ch_2/2_20.clj
@@ -1,18 +1,18 @@
 (ns sicp-clj.ch-2.2-20
-  (:require [sicp-clj.ch-2.list :refer [car, cdr, append]]))
+  (:require [sicp-clj.ch-2.list :refer [car, cdr]]))
 
 (defn same-parity [f & r]
-  (defn same-parity-it [l rs]
+  (defn helper "Predicate and remained list." [p l]
     (if (empty? l)
-      rs
-      (let [cur (car l)
-            both-odd (and (odd? f) (odd? cur))
-            both-even (and (even? f) (even? cur))]
-        (same-parity-it (cdr l)
-                (if (or both-even both-odd)
-                  (append rs (list cur))
-                  rs)))))
-  (cons f (same-parity-it r ())))
+      ()
+      (let [cur (car l)]
+        (cond
+          (p cur) (cons cur (helper p (cdr l)))
+          :else (helper p (cdr l))))))
+  (cons f
+        (cond
+          (even? f) (helper even? r)
+          (odd? f) (helper odd? r))))
 
 (println (same-parity 1 2 3 4 5 6 7))
 (println (same-parity 2 3 4 5 6 7))
